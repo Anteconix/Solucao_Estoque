d0ebaf57b535ac7559c892dda6fa57e51ede0c05
magnofilho2323@gmail.com <magnofilho2323@gmail.com>
2024-03-12 21:32:32 -0300

    Teste com objeto anônimo.

--- Teste/ClienteTeste.cs
+++ Teste/ClienteTeste.cs
@@ -1,9 +1,54 @@
 using Dominio;
+using ExpectedObjects;
+using System.Runtime.ConstrainedExecution;
 
 namespace Teste
 {
     public class ClienteTeste
     {
+        // Exemplo 02
+        // Teste usando objeto anônimo, ExpectedObject com apenas um assert
+
+        [Fact]
+        public void CriarObjetoCliente()
+        {
+            // Arrange
+            // criando um objeto anônimo
+            var clienteEsperado = new
+            {
+                Codigo = 1,
+                Nome = "Funeraria Vá com Deus",
+                Endereco = "Rua das Cacaia, 12",
+                Telefone = "(47) 3025-8076"
+            };
+
+            // Act
+            Cliente cli = new Cliente(
+                clienteEsperado.Codigo,
+                clienteEsperado.Nome,
+                clienteEsperado.Endereco,
+                clienteEsperado.Telefone
+                );
+
+            // Assert
+            clienteEsperado.ToExpectedObject().ShouldMatch( cli );
+
+
+        }
+
+        
+
+
+
+
+
+
+
+
+        /*
+        /* Exemplo 01
+         Teste simples, usando variáveis, avaliando usando vários asserts
+
         [Fact]
         public void CriarObjetoCliente()
         {
@@ -22,5 +67,6 @@ namespace Teste
             Assert.Equal(Endereco, cli.Endereco);
             Assert.Equal(Telefone, cli.Telefone);
         }
+        */
     }
 }
